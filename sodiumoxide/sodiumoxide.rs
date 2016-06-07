179aa1849901a6863eefc86f668cf514634ce04e
John Heitmann <jheitmann@gmail.com>
2016-06-07 00:30:56 -0700

    Updated to use new AES-GCM and ChaCha20-Poly1305 aead APIs in libsodium

--- sodiumoxide/sodiumoxide.rs
+++ sodiumoxide/sodiumoxide.rs
@@ -7,6 +7,8 @@ extern crate crypto_bench;
 
 extern crate sodiumoxide;
 
+mod aead;
+
 mod digest {
     macro_rules! sodiumoxide_digest_benches {
         ( $name:ident, $block_len:expr, $output_len:expr, $digester:path) => {
@@ -27,45 +29,4 @@ mod digest {
     sodiumoxide_digest_benches!(
         sha512, crypto_bench::SHA512_BLOCK_LEN, crypto_bench::SHA512_OUTPUT_LEN,
         sodiumoxide::crypto::hash::sha512::hash);
-}
-
-mod aead {
-    use test;
-
-    macro_rules! sodiumoxide_aead_bench {
-        ( $benchmark_name:ident, $input_len:expr, $sealer:path ) => {
-            #[bench]
-            fn $benchmark_name(b: &mut test::Bencher) {
-                use $sealer;
-                use sodiumoxide::crypto::secretbox;
-                b.bytes = $input_len;
-                let key = secretbox::gen_key();
-                let nonce = secretbox::gen_nonce();
-                // XXX: secretbox doesn't support in place encryption, so
-                // we explicitly memcpy to have the same overhead as
-                // other benchmarks.
-                // secretbox will add at most MACBYTES to the result length,
-                // so that lets us know how to build a buffer big enough
-                // for both input and output.
-                let mut in_out = vec![0u8; $input_len + secretbox::MACBYTES];
-                b.iter(|| {
-                    let out = seal(&in_out[0..$input_len], &nonce, &key);
-                    for i in 0..out.len() {
-                        in_out[i] = out[i];
-                    }
-                    out
-                });
-            }
-        }
-    }
-
-    sodiumoxide_aead_bench!(
-        xsalsa20poly1305_16, 16,
-        sodiumoxide::crypto::secretbox::xsalsa20poly1305::seal);
-    sodiumoxide_aead_bench!(
-        xsalsa20poly1305_1350, 1350,
-        sodiumoxide::crypto::secretbox::xsalsa20poly1305::seal);
-    sodiumoxide_aead_bench!(
-        xsalsa20poly1305_8192, 8192,
-        sodiumoxide::crypto::secretbox::xsalsa20poly1305::seal);
 }
