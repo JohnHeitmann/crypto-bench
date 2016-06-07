fd911ec39bef7374c8101b6b9dad685d8bf97eb8
John Heitmann <jheitmann@gmail.com>
2016-06-07 13:49:17 -0700

    Tweaks

--- sodiumoxide/aead.rs
+++ sodiumoxide/aead.rs
@@ -12,13 +12,13 @@ macro_rules! sodiumoxide_seal_in_place_bench {
             let nonce = _sealer::gen_nonce();
             // XXX: sodiumoxide doesn't support in place encryption, so
             // we explicitly memcpy to have the same overhead as
-            // other benchmarks.
-            // encrypt will add at most MACBYTES to the result length,
-            // so that lets us know how to build a buffer big enough
-            // for both input and output.
+            // other benchmarks. encrypt will add at most MACBYTES to the
+            // result length, so that lets us know how to build a buffer
+            // big enough for both input and output.
             let mut in_out = vec![0u8; $input_len + _sealer::MACBYTES];
             b.iter(|| {
-                let out = _sealer::encrypt(&in_out[0..$input_len], $ad, &nonce, &key);
+                let out = _sealer::encrypt(&in_out[0..$input_len],
+                                           $ad, &nonce, &key);
                 for i in 0..out.len() {
                     in_out[i] = out[i];
                 }
@@ -61,5 +61,7 @@ macro_rules! sodiumoxide_aead_benchs {
     }
 }
 
-sodiumoxide_aead_benchs!(chacha20_poly1305, sodiumoxide::crypto::aead::chacha20poly1305);
-sodiumoxide_aead_benchs!(aes_256_gcm, sodiumoxide::crypto::aead::aes256gcm);
+sodiumoxide_aead_benchs!(chacha20_poly1305,
+                         sodiumoxide::crypto::aead::chacha20poly1305);
+sodiumoxide_aead_benchs!(aes_256_gcm,
+                         sodiumoxide::crypto::aead::aes256gcm);
